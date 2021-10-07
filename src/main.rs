20026453e3cdd0d78883100390903395d2c11297
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-07 23:30:45 +0900

    feat: POSTメソッドのときはテンプレートエンジンを使ってレンダリングする関数で処理する

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Request, Response, Server};
+use hyper::{Body, Error, Method, Request, Response, Server};
 use std::{convert::Infallible, net::SocketAddr};
+use tera::{Context, Tera};
+
+static TEMPLATE: &str = "Hello, {{name}}!";
 
 async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World!".into()))
 }
 
 async fn route(req: Request<Body>) -> Result<Response<Body>, Error> {
-    handle(req).await.map_err(|e| match e {})
+    match *req.method() {
+        Method::POST => handle_with_body(req).await,
+        _ => handle(req).await.map_err(|e| match e {}),
+    }
+}
+
+async fn handle_with_body(req: Request<Body>) -> Result<Response<Body>, Error> {
+    let body = hyper::body::to_bytes(req.into_body()).await?;
+
+    let body = std::str::from_utf8(&body).unwrap();
+    let name = body.strip_prefix("name=").unwrap();
+
+    let mut tera = Tera::default();
+    tera.add_raw_template("hello", TEMPLATE).unwrap();
+    let mut ctx = Context::new();
+    ctx.insert("name", name);
+    let rendered = tera.render("hello", &ctx).unwrap();
+
+    Ok(Response::new(rendered.into()))
 }
 
 #[tokio::main]
