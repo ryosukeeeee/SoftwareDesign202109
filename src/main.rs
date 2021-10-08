79110ae5033fa93dc65c3b6ceadb32b4ad272ad8
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-08 23:21:38 +0900

    feat: TeraをArcで持ち回るようにした

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Error, Method, Request, Response, Server};
+use std::sync::Arc;
 use std::{convert::Infallible, net::SocketAddr};
 use tera::{Context, Tera};
 
@@ -9,21 +10,19 @@ async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World!".into()))
 }
 
-async fn route(req: Request<Body>) -> Result<Response<Body>, Error> {
+async fn route(req: Request<Body>, tera: Arc<Tera>) -> Result<Response<Body>, Error> {
     match *req.method() {
-        Method::POST => handle_with_body(req).await,
+        Method::POST => handle_with_body(req, tera).await,
         _ => handle(req).await.map_err(|e| match e {}),
     }
 }
 
-async fn handle_with_body(req: Request<Body>) -> Result<Response<Body>, Error> {
+async fn handle_with_body(req: Request<Body>, tera: Arc<Tera>) -> Result<Response<Body>, Error> {
     let body = hyper::body::to_bytes(req.into_body()).await?;
 
     let body = std::str::from_utf8(&body).unwrap();
     let name = body.strip_prefix("name=").unwrap();
 
-    let mut tera = Tera::default();
-    tera.add_raw_template("hello", TEMPLATE).unwrap();
     let mut ctx = Context::new();
     ctx.insert("name", name);
     let rendered = tera.render("hello", &ctx).unwrap();
@@ -35,7 +34,14 @@ async fn handle_with_body(req: Request<Body>) -> Result<Response<Body>, Error> {
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(route)) });
+    let mut tera = Tera::default();
+    tera.add_raw_template("hello", TEMPLATE).unwrap();
+    let tera = Arc::new(tera);
+
+    let make_svc = make_service_fn(|_conn| {
+        let tera = tera.clone();
+        async { Ok::<_, Infallible>(service_fn(move |req| route(req, tera.clone()))) }
+    });
     let server = Server::bind(&addr).serve(make_svc);
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
